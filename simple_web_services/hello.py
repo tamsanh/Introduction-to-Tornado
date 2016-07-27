879eb0a32a4f8bcf06b9c30cbfdddfc6dc1c99ac
Tam Nguyen <tamn@palantir.com>
2016-07-27 14:50:49 -0400

    Fix formatting

--- simple_web_services/hello.py
+++ simple_web_services/hello.py
@@ -4,16 +4,22 @@ import tornado.options
 import tornado.web
 
 from tornado.options import define, options
-define("port", default=8888, help="run on the given port", type=int)
+
+define("port", default=9999, help="run on the given port", type=int)
+
 
 class IndexHandler(tornado.web.RequestHandler):
-	def get(self):
-		greeting = self.get_argument('greeting', 'Hello')
-		self.write(greeting + ', friendly user!')
+    def data_received(self, chunk):
+        return super(IndexHandler, self).data_received(chunk)
+
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
 
 if __name__ == "__main__":
-	tornado.options.parse_command_line()
-	app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
-	http_server = tornado.httpserver.HTTPServer(app)
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.instance().start()
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
